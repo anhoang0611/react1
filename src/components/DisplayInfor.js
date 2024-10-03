23f398531ffd89430ef44e097496cef527f0d512
anhoang0611 <94751169+anhoang0611@users.noreply.github.com>
2024-10-03 17:58:01 +0700

    life cycle react

--- src/components/DisplayInfor.js
+++ src/components/DisplayInfor.js
@@ -2,6 +2,31 @@ import React from "react";
 import './DisplayInfor.scss'
 import logo from './../logo.svg'
 class DisplayInfor extends React.Component {
+    constructor(props) {
+        console.log("call me constructor: 1")
+        super(props);
+        //babel compiler
+        this.state = {
+            isShowListUser: true
+        }
+    }
+
+    componentDidMount() {
+        console.log('>>> Call me component did mount')
+        setTimeout(() => {
+            document.title = 'Rain'
+        }, 3000);
+    }
+
+    componentDidUpdate(prevPros, prevState, snapshot) {
+        console.log('>>> call  me component did Update', this.props, prevPros)
+        if (this.props.listUsers !== prevPros.listUsers) {
+            if (this.props.listUsers.length === 5) {
+                alert('you got 5 user')
+            }
+        }
+    }
+
     state = {
         isShowListUser: true
     }
@@ -11,7 +36,10 @@ class DisplayInfor extends React.Component {
         })
     }
 
+
+
     render() {
+        console.log("call me render")
         // destructuring array/obj
         const { listUsers } = this.props;
         console.log(listUsers)
