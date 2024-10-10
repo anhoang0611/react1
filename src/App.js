7413852a410f6fabcdf4a9d1e3a9086ba2cede44
anhoang0611 <94751169+anhoang0611@users.noreply.github.com>
2024-10-10 18:16:11 +0700

    setup project

--- src/App.js
+++ src/App.js
@@ -1,21 +1,15 @@
-import logo from './logo.svg';
 import './App.scss';
-import { useDispatch, useSelector } from 'react-redux';
-import { increaseCounter, decreaseCounter } from './redux/action/counterAction';
 
-import MyComponent from './components/MyComponent';
-import React from 'react';
 
-class App extends React.Component {
 
-  render() {
-    return (
-      <div className='app-container'>
-        Hello world &amp; Who ask
-        <MyComponent></MyComponent>
-      </div>
-    );
-  }
+
+const App = () => {
+  return (
+    <div className="app-container">
+      hello world
+
+    </div>
+  );
 }
 
 
