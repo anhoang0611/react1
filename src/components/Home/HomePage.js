919026ad27b209c44208fbb13a7a591b39e76de4
anhoang0611 <94751169+anhoang0611@users.noreply.github.com>
2024-10-12 04:13:33 +0700

    add video for website

--- src/components/Home/HomePage.js
+++ src/components/Home/HomePage.js
@@ -1,7 +1,15 @@
+import videohomepage from '../../assets/videohomepage.mp4';
 const HomePage = (props) => {
     return (
         <div className='homepage-container'>
-            homepage component
+            <video autoPlay muted loop >
+                <source
+                    src={videohomepage}
+                    type="video/mp4"
+                />
+
+
+            </video>
         </div>
     )
 }
