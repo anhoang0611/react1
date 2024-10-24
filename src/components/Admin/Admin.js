5c331bd5c50dab21fa0999c18da0866f732402dc
anhoang0611 <94751169+anhoang0611@users.noreply.github.com>
2024-10-24 19:48:32 +0700

    validate user and notify

--- src/components/Admin/Admin.js
+++ src/components/Admin/Admin.js
@@ -4,6 +4,8 @@ import { FaBars } from 'react-icons/fa';
 import { useState } from "react";
 import { Outlet } from "react-router-dom";
 
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const Admin = (props) => {
     const [collapsed, setCollapsed] = useState(false);
@@ -24,6 +26,19 @@ const Admin = (props) => {
                 </div>
 
             </div>
+
+            <ToastContainer
+                position="top-center"
+                autoClose={5000}
+                hideProgressBar={false}
+                newestOnTop={false}
+                closeOnClick
+                rtl={false}
+                pauseOnFocusLoss
+                draggable
+                pauseOnHover
+
+            />
         </div>
     )
 }
