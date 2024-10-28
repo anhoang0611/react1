123074afee6aaa98616c358a8da2f093e1c00974
anhoang0611 <94751169+anhoang0611@users.noreply.github.com>
2024-10-28 20:16:27 +0700

    update list user

--- src/components/Admin/Content/ManageUser.js
+++ src/components/Admin/Content/ManageUser.js
@@ -1,13 +1,36 @@
 import ModalCreateUsers from "./ModalCreateUser";
 import './ManageUser.scss';
 import { FcPlus } from "react-icons/fc";
-import { useState } from "react";
 import TableUser from "./TableUser";
+import { useEffect, useState } from "react";
+import { getAllUsers } from "../../../services/apiServices";
 
 const ManageUsers = (props) => {
     const [showModalCreateUser, setShowModalCreateUser] = useState(false);
 
+    const [listUsers, setListUsers] = useState([])
 
+    //componentDidMount
+    useEffect(() => {
+        fetchListUsers();
+    }, []);
+
+
+    const fetchListUsers = async () => {
+        let res = await getAllUsers();
+
+        if (res.EC === 0) {
+            setListUsers(res.DT)
+        }
+    }
+
+    const testfunction = async () => {
+        let res = await getAllUsers();
+
+        if (res.EC === 0) {
+            setListUsers(res.DT)
+        }
+    }
 
     return (
         <div className="manage-user-container">
@@ -25,11 +48,12 @@ const ManageUsers = (props) => {
                 </div>
 
                 <div className="table-users-container">
-                    <TableUser />
+                    <TableUser listUsers={listUsers} />
                 </div>
                 <ModalCreateUsers
                     show={showModalCreateUser}
                     setShow={setShowModalCreateUser}
+                    fetchListUsers={fetchListUsers}
                 />
             </div>
 
--- src/components/Admin/Content/TableUser.js
+++ src/components/Admin/Content/TableUser.js
@@ -1,31 +1,15 @@
-import { useEffect, useState } from "react";
-import { getAllUsers } from "../../../services/apiServices";
-const TableUser = () => {
 
-    const [listUsers, setListUsers] = useState([
+const TableUser = (props) => {
 
-    ])
+    const { listUsers } = props;
 
-    //componentDidMount
-    useEffect(() => {
-        fetchListUsers();
-    }, []);
-
-
-    const fetchListUsers = async () => {
-        let res = await getAllUsers();
-
-        if (res.EC === 0) {
-            setListUsers(res.DT)
-        }
-    }
 
     return (
         <>
             <table className="table table-hover table-bordered">
                 <thead>
                     <tr>
-                        <th scope="col">No</th>
+                        <th scope="col">ID</th>
                         <th scope="col">Username</th>
                         <th scope="col">Email</th>
                         <th scope="col">Role</th>
@@ -38,7 +22,7 @@ const TableUser = () => {
                         listUsers.map((item, index) => {
                             return (
                                 <tr key={`table-users-${index}`}>
-                                    <td>{index + 1}</td>
+                                    <td>{item.id}</td>
                                     <td>{item.username}</td>
                                     <td>{item.email}</td>
                                     <td>{item.role}</td>
